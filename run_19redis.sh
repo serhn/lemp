fc81a24739d8715c0af07c3d1b81a5649a7bd9b4
Serhii Nikolaichuk <serh@top.radio>
2019-01-12 15:08:27 +0200

    add redis

---
+++ run_19redis.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+. inc/init.sh
+IMAGE=redis
+
+
+#ADD_LINKS="memcache php56" 
+ADD_LINKS="" 
+#check_link
+echo $LINKS
+
+docker run  -d \
+            --restart unless-stopped  \
+            --name="$CONTAINER" \
+            $LINKS \
+            $IMAGE
